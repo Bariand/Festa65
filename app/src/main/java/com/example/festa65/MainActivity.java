528bec0d53d0fd4381b750b8f1e49b69be101264
Lucas Freitas Cordeiro <93345102+Bariand@users.noreply.github.com>
2025-08-29 15:19:30 -0300

    Initial commit

---
+++ app/src/main/java/com/example/festa65/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.festa65;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    // Coordenadas do local da festa (exemplo: Pelotas-RS)
+    private static final LatLng LOCAL_FESTA = new LatLng(-31.771199, -52.342900);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.addMarker(new MarkerOptions().position(LOCAL_FESTA).title("Local da Festa"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LOCAL_FESTA, 15f));
+    }
+}
